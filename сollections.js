2fd1fa402f29fcfebc407066fba933d0586fb210
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-10-09 15:16:52 +0300

    refactor: replaced images in collections

--- сollections.js
+++ сollections.js
@@ -2,7 +2,7 @@ const сollections = [
 	{
 		id: '1с',
 		imageUrl:
-			'https://downloader.disk.yandex.ru/preview/d91b71a9defa4eea58535f006fdb00552820cf01c43135f131b2f046d6318a58/6516e60e/0SQZ2JFG1AWf_FIoT_jrO1q3iDO_NzRCN62m_I4v8Ov-BvSJ9eFj1a0R4c6eedt41Wuc3me_KPiv7G--irXsqQ%3D%3D?uid=0&filename=classic3.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
+			'https://res.cloudinary.com/dsoxasi6c/image/upload/v1696853501/collections/oyxchajbvzno176vodoe.jpg',
 		alt: 'Классическая коллекция',
 		name: 'Классика',
 		collectionName: 'classic',
@@ -11,7 +11,7 @@ const сollections = [
 	{
 		id: '2с',
 		imageUrl:
-			'https://downloader.disk.yandex.ru/preview/a4dbf00e38a15684b3c81f03733fd568ae1847011fc68dd10e6799709d4de7e6/6516e633/p9vh7bn1sJmSjaTEaHsSTB7Ay4ESlkLQojBHmB2I3vz8Vlll3LlSdPvuWsLLUK1n9IXx-frW6lUsRDXWMxFQXA%3D%3D?uid=0&filename=neoclassic.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
+			'https://res.cloudinary.com/dsoxasi6c/image/upload/v1696853501/collections/zvk2hxtrs2elkjtccfxv.jpg',
 		alt: 'Неоклассика коллекция',
 		name: 'Неоклассика',
 		collectionName: 'neoclassic',
@@ -20,7 +20,7 @@ const сollections = [
 	{
 		id: '3c',
 		imageUrl:
-			'https://downloader.disk.yandex.ru/preview/d4f08c8d4164af6d0379e7a7db5952ba28b5c3dbecc937417e3a7383f16579e6/6516e657/4cf85L5UxdUNXTqToI3r1h7Ay4ESlkLQojBHmB2I3vyOO-rQeLfr-t5-FtEdB6uYewiMcH2caO805Cq0-PCo6w%3D%3D?uid=0&filename=modern1.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
+			'https://res.cloudinary.com/dsoxasi6c/image/upload/v1696853500/collections/e1rqy7mtpwtgiuwfpzux.jpg',
 		alt: 'Современная коллекция',
 		name: 'Современный стиль',
 		collectionName: 'modern style',
@@ -29,7 +29,7 @@ const сollections = [
 	{
 		id: '4c',
 		imageUrl:
-			'https://downloader.disk.yandex.ru/preview/cdfe40f29045758890df2580c2eeec2368882917e01a6539700c1e435b2f443a/6516e6de/cjq4fqxFnQ7WDGjBDsabYAUBZYecBIZoBobZMuzWMISbLGPEDPySZ-PvtIq0mrUJz3ugtHZ_WmLPfqt2eq23jA%3D%3D?uid=0&filename=razd2.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
+			'https://res.cloudinary.com/dsoxasi6c/image/upload/v1696853502/collections/x1cafagtic5xi43wrqfb.jpg',
 		alt: 'Коллекция раздвижных перегородок',
 		name: 'раздвижные перегородки',
 		collectionName: 'sliding partitions',
